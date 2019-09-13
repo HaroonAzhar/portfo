8e07bbe31a13d2b7d5128b3636d7bc3569a7aa5d
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-09-13 06:18:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+import Carousel from './components/Carousel'
+import './App.css'
+import './css/open-iconic-bootstrap.min.css'
+import './css/animate.css'
+import './css/owl.carousel.min.css'
+import './css/owl.theme.default.min.css'
+import './css/magnific-popup.css'
+import './css/aos.css'
+import './css/ionicons.min.css'
+import './css/flaticon.css'
+import './css/icomoon.css'
+import './css/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*import './js/jquery.min.js'
+
+import './js/jquery-migrate-3.0.1.min.js'
+import './js/popper.min.js'
+import './js/bootstrap.min.js'
+import './js/jquery.easing.1.3.js'
+import './js/jquery.waypoints.min.js'
+import './js/jquery.stellar.min.js'
+import './js/owl.carousel.min.js'
+import './js/jquery.magnific-popup.min.js'
+import './js/aos.js'
+import './js/jquery.animateNumber.min.js'
+import './js/scrollax.min.js'
+import './js/main.js'*/
+
+
+    const script = document.createElement("script");
+
+    script.src = "https://unpkg.com/ionicons@4.2.2/dist/ionicons.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+
+    
+class App extends Component {
+  render() {
+    return (
+         
+         <div className="App">
+          
+           <Navbar/>
+           <Carousel/>
+           
+          </div>
+         
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+i
+
+className Home extends Component {
+  render(){
+    
+      
+        return 
+        (
+           
+        );
+          
+    
+  }
+}
+
+
+export default Home
